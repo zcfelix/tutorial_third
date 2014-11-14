eba094e624ad7ffb84a4d476403457739922799a
zcfelix <felix-zhou@FelixZhoudeMacBook-Air.local>
2014-11-14 09:25:25 +0800

    Finish exercises

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Two following lines added by Felix 3rd, Nov 2014
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
